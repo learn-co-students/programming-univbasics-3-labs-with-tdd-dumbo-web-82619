94eeed1f09e3d8fd79582a779bbaf76e31debcac
Karina Macancela <karymacancela@gmail.com>
2019-08-15 14:58:32 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
 # Add your variables here
+
+# First test
+first_number = 0
+
+# Second test
+second_number = 1
+
+# Third test
+sum = first_number + second_number
+
+# Fourth, fifth, and sixth tests
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
